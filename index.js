ef2298a6d817123e9da0d6d3e259b404e4db80e5
Zachary M <101447294+zach89129@users.noreply.github.com>
2022-09-17 07:57:28 -0700

    heroku changes

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: "GET,POST,PUT,DELETE",
+//     credentials: true,
+//   })
+// );
 
 //routes use
 app.use("/", userRoutes);
